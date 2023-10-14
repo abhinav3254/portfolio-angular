cf8ea43bc2ab1d6f83bd85871adc20a1a659c931
abhinav3254 <abhinav3254@gmail.com>
2023-10-14 18:07:07 +0530

    skills done

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -7,4 +7,83 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
 
+  skills = [
+    {
+      "name": "Java",
+      "src": "assets/images/java-horizontal.svg"
+    },
+    {
+      "name": "Android",
+      "src": "assets/images/android-ar21.svg"
+    },
+    {
+      "name": "Angular",
+      "src": "assets/images/angular-ar21.svg"
+    },
+    {
+      "name": "Flutter",
+      "src": "assets/images/flutterio-ar21.svg"
+    },
+    {
+      "name": "GitHub",
+      "src": "assets/images/github-ar21.svg"
+    },
+    {
+      "name": "Hibernate",
+      "src": "assets/images/hibernate.svg"
+    },
+    {
+      "name": "JavaScript",
+      "src": "assets/images/javascript-horizontal.svg"
+    },
+    {
+      "name": "Linux",
+      "src": "assets/images/linux.svg"
+    },
+    {
+      "name": "MongoDB",
+      "src": "assets/images/mongodb-ar21.svg"
+    },
+    {
+      "name": "MySQL",
+      "src": "assets/images/mysql-horizontal.svg"
+    },
+    {
+      "name": "Node.js",
+      "src": "assets/images/node.svg"
+    },
+    {
+      "name": "Open Source",
+      "src": "assets/images/opensource.svg"
+    },
+    {
+      "name": "Spring Framework",
+      "src": "assets/images/springio.svg"
+    },
+    {
+      "name": "SQLite",
+      "src": "assets/images/sqlite-ar21.svg"
+    },
+    {
+      "name": "CSS",
+      "src": "assets/images/w3_css-ar21.svg"
+    },
+    {
+      "name": "HTML5",
+      "src": "assets/images/w3_html5-ar21.svg"
+    },
+    {
+      "name": "Azure",
+      "src": "assets/images/microsoft_azure-ar21.svg"
+    },
+    {
+      "name": "Microservice",
+      "src": "assets/images/microservice-icon.svg"
+    }
+  ]
+
+
+
+
 }
+
