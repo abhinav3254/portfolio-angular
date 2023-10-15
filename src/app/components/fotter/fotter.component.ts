e92995952563036218381e9323eebb5ad9df1566
abhinav3254 <abhinav3254@gmail.com>
2023-10-15 16:14:41 +0530

    responsive

--- src/app/components/fotter/fotter.component.ts
+++ src/app/components/fotter/fotter.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class FotterComponent {
 
+
+  socials = [
+    {
+      name: 'instagram',
+      src: 'assets/social/instagram-icon.svg',
+      link: 'https://www.instagram.com/__abhinav__jha__'
+    },
+    {
+      name: 'github',
+      src: 'assets/social/github-icon.svg',
+      link: 'https://github.com/abhinav3254'
+    },
+    {
+      name: 'linkedin',
+      src: 'assets/social/linkedin-icon.svg',
+      link: 'https://www.linkedin.com/in/abhinav3254'
+    },
+    {
+      name: 'stackoverflow',
+      src: 'assets/social/stackoverflow-icon.svg',
+      link: 'https://stackoverflow.com/users/22455030/abhinav-jha'
+    },
+    {
+      name: 'twitter',
+      src: 'assets/social/twitter-icon.svg',
+      link: 'https://twitter.com/abhinavjha3254'
+    }
+  ]
 }
